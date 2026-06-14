3b0c3f3ac063cc531fc49c5a71ce18d7887f6d8a
PEN <uemasaki20011222@gmail.com>
2026-06-14 20:09:19 +0900

    picoquic

--- picoquic/build/picoquic/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/WorkSpace/GitHub/hello-picoquic/picoquic
-# Build directory: D:/WorkSpace/GitHub/hello-picoquic/build/picoquic
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/picotls-build")
